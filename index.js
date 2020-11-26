9d7021506a450123a8eef783a455511c89c36cea
Holly Johnson <hollyj@hotmail.co.uk>
2020-11-26 18:49:23 +0000

    up to 12

--- index.js
+++ index.js
@@ -11,3 +11,150 @@
 'use strict';
 
 // add your functions here
+
+function replaceText(elem, str) {
+    elem.textContent = str;
+};
+
+function addTextTo(elem, str) {
+    elem.append(str);
+};
+
+function moreBears() {
+    animals.src = "http://placebear.com/400/200";
+    animals.alt = "A bear.";
+    animals.title = "A BEAR!";
+};
+
+function setId(elem, str) {
+    elem.id = str;
+    return elem;
+};
+
+function setClass(elem, str) {
+    elem.className = str;
+    return elem;
+};
+
+function addAClass(elem, str) {
+    elem.classList.add(str);
+    return elem;
+};
+
+function removeAClass(elem, str) {
+    elem.classList.remove(str);
+};
+
+function newElement(name) {
+    const x = document.createElement(name);
+    return x;
+};
+
+function findElementById(id) {
+    const x = document.querySelector(`#${id}`);
+    return x;
+}
+
+function findElementsByQuery(query) {
+    const x = document.querySelectorAll(query);
+    return x;
+}
+
+function reverseList(query) {
+    const list = document.querySelector(query);
+    const items = list.children;
+
+    for (let i = items.length - 1; i >= 0; i--) {
+        list.append(items[i]);
+    }
+
+    return list;
+}
+
+function mover(moveThis, appendToThis) {
+    const item = document.querySelector(moveThis);
+    const dest1 = document.querySelector(appendToThis);
+    dest1.appendChild(item);
+}
+
+function filler(list, candidates) {
+console.log(candidates);
+//console.log(list);
+if (!candidates) return;
+    const items = document.querySelectorAll(`#${candidates}`);
+   // const destination = document.querySelector(`#${list}`);
+
+    for (const x of items) {
+        destination.appendChild(x);
+    }
+
+
+     /*
+
+ const items = document.querySelectorAll(candidates);
+    const destination = document.querySelector(list);
+
+    for (const x of items) {
+        destination.appendChild(x);
+    }
+
+
+      */
+
+
+
+
+   /*
+   for (const x of candidates) {
+       list.appendChild(x);
+   }
+
+    */
+}
+
+/*
+ const paras = document.querySelectorAll("p");
+  const dest1 = document.querySelector("#dest1");
+
+  for (const p of paras) {
+    dest1.appendChild(p);
+  }
+
+  // move one element to destination two
+  const dest2 = document.querySelector("#dest2");
+  const moveme = document.querySelector("#moveme");
+  dest2.appendChild(moveme);
+</script>
+
+ */
+
+
+
+
+function dupe(selector) {
+    const item = document.querySelectorAll(selector);
+    const copiedItem = item;
+    selector.appendChild(copiedItem);
+}
+
+function removeAll(selector) {
+    const item = document.querySelectorAll(selector);
+
+    for (const x of item) {
+        item.remove();
+    }
+}
+
+/*
+
+<ul id=x>
+  <li>one
+  <li>two
+  <li id=four>four
+<ul>
+
+<script>
+  const four = document.querySelector("#four");
+  four.remove();
+
+ */
